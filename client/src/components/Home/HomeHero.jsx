293dba9ebc2fddbff0400d22718ca689f434a8dd
d3vote <65717310+d3vote@users.noreply.github.com>
2023-06-10 00:40:23 +0200

    small bugfix of yusufs text

--- client/src/components/Home/HomeHero.jsx
+++ client/src/components/Home/HomeHero.jsx
@@ -12,12 +12,7 @@ const HomeGame = () => {
   const [incorrectLetters, setIncorrectLetters] = useState([]);
 
   const text =
-    `Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-  nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-  erat, sed diam voluptua. At vero eos et accusam et justo duo
-  dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
-  sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
-  amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor`.split(" ");
+    `Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor`.split(" ");
 
   const handleClickForBlur = () => {
     setIsBlurred(false);
