7986bd6799d72cac1c9dfde343fa7790e96eef14
Yunus Kara <yunuskr40@gmail.com>
2023-06-14 20:14:53 +0200

    worked on the settings/added endpoints in server

--- client/src/pages/Settings.jsx
+++ client/src/pages/Settings.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { BiTimeFive } from "react-icons/bi";
+import Navbar from "../components/Home/Navbar.jsx";
 import "../styles/Settings.css";
 
 function Settings() {
@@ -9,10 +11,73 @@ function Settings() {
   const [newEmail, setNewEmail] = useState("");
   const [newPassword, setNewPassword] = useState("");
 
-  const handleDeleteAccount = () => {};
-  const handleUpdateEmail = () => {};
-  const handleUpdatePassword = () => {};
-  const handleUpdateUsername = () => {};
+  const handleDeleteAccount = async () => {
+    try {
+      // Call the delete account API endpoint
+      await fetch("/api/delete-account", {
+        method: "DELETE",
+        // Add necessary headers or authentication tokens if required
+      });
+
+      // Account deletion successful, perform any additional cleanup or redirection
+    } catch (error) {
+      setError("Failed to delete the account. Please try again later.");
+    }
+  };
+
+  const handleUpdateEmail = async () => {
+    try {
+      // Call the update email API endpoint with the new email value
+      await fetch("/api/update-email", {
+        method: "POST",
+        body: JSON.stringify({ email: newEmail }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+        // Add necessary headers or authentication tokens if required
+      });
+
+      // Email update successful, perform any additional actions
+    } catch (error) {
+      setError("Failed to update the email. Please try again later.");
+    }
+  };
+
+  const handleUpdatePassword = async () => {
+    try {
+      // Call the update password API endpoint with the new password value
+      await fetch("/api/update-password", {
+        method: "POST",
+        body: JSON.stringify({ password: newPassword }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+        // Add necessary headers or authentication tokens if required
+      });
+
+      // Password update successful, perform any additional actions
+    } catch (error) {
+      setError("Failed to update the password. Please try again later.");
+    }
+  };
+
+  const handleUpdateUsername = async () => {
+    try {
+      // Call the update username API endpoint with the new username value
+      await fetch("/api/update-username", {
+        method: "POST",
+        body: JSON.stringify({ username: newUsername }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+        // Add necessary headers or authentication tokens if required
+      });
+
+      // Username update successful, perform any additional actions
+    } catch (error) {
+      setError("Failed to update the username. Please try again later.");
+    }
+  };
 
   const handleToggleAvatarOptions = () => {
     setShowAvatarOptions(!showAvatarOptions);
@@ -28,57 +93,69 @@ function Settings() {
   };
 
   return (
-    <div className="main-bg flex items-center justify-center h-screen">
-      <div className="container mx-auto bg-gray-600 p-8 rounded-lg">
-        <div className="max-w-sm mx-auto mt-10">
-          <div className="flex items-center mb-4">
-            <div className="w-16 h-16 rounded-full overflow-hidden mr-4">
-              {avatarStyle && <img src={avatarStyle} alt="Avatar" />}
+    <>
+      <div className="main-bg flex flex-col items-center justify-center h-screen">
+        <div className="h-[90%] m-auto max-w-7xl">
+          <Navbar />
+          <h1 className="text-white mt-8 text-xl pb-7">Settings</h1>
+          <div className="container mx-auto bg-gray-600 p-8 rounded-lg flex">
+            <div className="w-1/2">
+              <div className="flex items-center mb-4">
+                <div className="w-16 h-16 rounded-full overflow-hidden mr-4">
+                  {avatarStyle && <img src={avatarStyle} alt="Avatar" />}
+                </div>
+                <div>
+                  <p className="text-white">Username:</p>
+                  <button
+                    className="bg-black text-white font-semibold py-2 px-4 rounded mt-2"
+                    onClick={handleUpdateUsername}
+                  >
+                    Edit
+                  </button>
+                </div>
+              </div>
+
+              <div className="flex items-center">
+                <div>
+                  <p className="text-white">Email:</p>
+                  <button
+                    className="bg-black text-white font-semibold py-2 px-4 rounded mt-2"
+                    onClick={handleUpdateEmail}
+                  >
+                    Edit
+                  </button>
+                </div>
+              </div>
+
+              <div className="flex items-center">
+                <div>
+                  <p className="text-white">Password:</p>
+                  <button
+                    className="bg-black text-white font-semibold py-2 px-4 rounded mt-2"
+                    onClick={handleUpdatePassword}
+                  >
+                    Edit
+                  </button>
+                </div>
+              </div>
             </div>
-            <ul className="list-disc text-white">
-              <li>Username:</li>
-              <li>Email:</li>
-              <li>Password:</li>
-            </ul>
-            <div className="ml-auto">
-              <button
-                className="bg-black text-white font-semibold py-2 px-4 rounded"
-                onClick={handleUpdateUsername}
-              >
-                Edit
-              </button>
-              <p>Username</p>
+
+            <div className="w-1/2">
+              <div className="flex flex-col mt-4">
+                <button
+                  className="bg-black text-white font-semibold py-2 px-4 rounded"
+                  onClick={handleToggleAvatarOptions}
+                >
+                  Change Avatar
+                </button>
+                <button
+                  className="mt-4 bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded"
+                  onClick={() => handleDeleteAccount()}
+                >
+                  Delete Account
+                </button>
+              </div>
             </div>
-          </div>
-          <div className="flex items-center">
-            <div className="ml-auto">
-              <button
-                className="bg-black text-white font-semibold py-2 px-4 rounded"
-                onClick={handleUpdateEmail}
-              >
-                Edit
-              </button>
-              <button
-                className="ml-4 bg-black text-white font-semibold py-2 px-4 rounded"
-                onClick={handleUpdatePassword}
-              >
-                Edit
-              </button>
-            </div>
-          </div>
-          <div className="flex flex-col mt-4">
-            <button
-              className="bg-black text-white font-semibold py-2 px-4 rounded"
-              onClick={handleToggleAvatarOptions}
-            >
-              Change Avatar
-            </button>
-            <button
-              className="mt-4 bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded"
-              onClick={() => handleDeleteAccount()}
-            >
-              Delete Account
-            </button>
           </div>
           {showAvatarOptions && (
             <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-gray-800 bg-opacity-75">
@@ -187,8 +264,15 @@ function Settings() {
           )}
           {error && <div className="text-red-500 mt-4">{error}</div>}
         </div>
+        <div>
+          <a href="/">
+            <span className="ont-medium text-white transition-colors hover:text-gray-400">
+              Go Back
+            </span>
+          </a>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- server/server.js
+++ server/server.js
@@ -5,14 +5,19 @@ import {
   GoogleAuthProvider,
   signInWithPopup,
   signInWithEmailAndPassword,
+  deleteUser,
+  updateProfile,
+  updateEmail,
+  updatePassword,
 } from "firebase/auth";
 import {
-    getFirestore,
-    doc,
-    setDoc,
-    getDoc,
-    getDocs,
-    collection,
+  getFirestore,
+  doc,
+  setDoc,
+  getDoc,
+  getDocs,
+  collection,
+  deleteDoc,
 } from "firebase/firestore";
 import "./db/firebase.mjs";
 import cors from "cors";
@@ -152,74 +157,73 @@ app.post("/login", async (req, res) => {
   }
 });
 
-app.post('/validate', async (req, res) => {
-    const token = req.body.token;
-    if (token) {
-        try {
-            const decoded = jwt.verify(token, secretKey);
-            res.status(200).send({ valid: true, username: decoded.username });
-        } catch (e) {
-            res.status(401).send({ valid: false, error: e.message });
-        }
-    } else {
-        res.status(400).send({ valid: false, error: "No token provided" });
+app.post("/validate", async (req, res) => {
+  const token = req.body.token;
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, secretKey);
+      res.status(200).send({ valid: true, username: decoded.username });
+    } catch (e) {
+      res.status(401).send({ valid: false, error: e.message });
     }
+  } else {
+    res.status(400).send({ valid: false, error: "No token provided" });
+  }
 });
 
 app.get("/movies", async (req, res) => {
-    try {
-        const moviesRef = collection(db, "movies");
-        const moviesSnapshot = await getDocs(moviesRef);
+  try {
+    const moviesRef = collection(db, "movies");
+    const moviesSnapshot = await getDocs(moviesRef);
 
-        const moviesList = [];
-        moviesSnapshot.forEach((doc) => {
-            const movieData = {
-                title: doc.id,
-                poster: doc.data().poster,
-            };
-            moviesList.push(movieData);
-        });
+    const moviesList = [];
+    moviesSnapshot.forEach((doc) => {
+      const movieData = {
+        title: doc.id,
+        poster: doc.data().poster,
+      };
+      moviesList.push(movieData);
+    });
 
-        res.status(200).json(moviesList);
-    } catch (error) {
-        console.error("Error retrieving movie list:", error);
-        res.status(500).send({ error: error.message });
-    }
+    res.status(200).json(moviesList);
+  } catch (error) {
+    console.error("Error retrieving movie list:", error);
+    res.status(500).send({ error: error.message });
+  }
 });
 
 function decodeMovieName(encodedMovieName) {
-    let decodedMovieName = '';
-    try {
-        decodedMovieName = decodeURIComponent(encodedMovieName);
-    } catch (error) {
-        // Handle any decoding errors here
-        console.error('Error decoding movie name:', error);
-    }
-    return decodedMovieName;
+  let decodedMovieName = "";
+  try {
+    decodedMovieName = decodeURIComponent(encodedMovieName);
+  } catch (error) {
+    // Handle any decoding errors here
+    console.error("Error decoding movie name:", error);
+  }
+  return decodedMovieName;
 }
 
 app.get("/movies/:movie/levels/:level", async (req, res) => {
-    try {
-        const { movie, level } = req.params;
-        const decodedMovieName = decodeMovieName(movie);
-        const levelRef = doc(db, `movies/${decodedMovieName}/levels/lvl${level}`);
-        const levelSnapshot = await getDoc(levelRef);
+  try {
+    const { movie, level } = req.params;
+    const decodedMovieName = decodeMovieName(movie);
+    const levelRef = doc(db, `movies/${decodedMovieName}/levels/lvl${level}`);
+    const levelSnapshot = await getDoc(levelRef);
 
-        if (!levelSnapshot.exists()) {
-            res.status(404).send({ error: "Level not found" });
-            return;
-        }
-
-        const levelData = levelSnapshot.data();
-        const text = levelData.text;
-
-        res.status(200).json({ text: text });
-    } catch (error) {
-        console.error(`Error retrieving level for movie: `, error);
-        res.status(500).send({ error: error.message });
+    if (!levelSnapshot.exists()) {
+      res.status(404).send({ error: "Level not found" });
+      return;
     }
-});
 
+    const levelData = levelSnapshot.data();
+    const text = levelData.text;
+
+    res.status(200).json({ text: text });
+  } catch (error) {
+    console.error(`Error retrieving level for movie: `, error);
+    res.status(500).send({ error: error.message });
+  }
+});
 
 app.get("/:username", async (req, res) => {
   const { username } = req.params;
@@ -258,19 +262,74 @@ app.get("/:username", async (req, res) => {
 });
 
 app.get("/movies/:movie/countlevels", async (req, res) => {
-    try {
-        const { movie } = req.params;
-        const levelsRef = collection(db, `movies/${movie}/levels`);
-        const levelsSnapshot = await getDocs(levelsRef);
-        const levelsCount = levelsSnapshot.size;
+  try {
+    const { movie } = req.params;
+    const levelsRef = collection(db, `movies/${movie}/levels`);
+    const levelsSnapshot = await getDocs(levelsRef);
+    const levelsCount = levelsSnapshot.size;
 
-        res.status(200).json({ count: levelsCount });
-    } catch (error) {
-        console.error(`Error retrieving levels for movie ${movie}:`, error);
-        res.status(500).send({ error: error.message });
-    }
+    res.status(200).json({ count: levelsCount });
+  } catch (error) {
+    console.error(`Error retrieving levels for movie ${movie}:`, error);
+    res.status(500).send({ error: error.message });
+  }
 });
 
+app.post("/edit", async (req, res) => {
+  const { token, username, email, password } = req.body;
+
+  try {
+    const decoded = jwt.verify(token, secretKey);
+    const { uid } = decoded;
+
+    // Update user profile
+    await updateProfile(auth.currentUser, {
+      displayName: username,
+    });
+
+    // Update user email
+    if (email) {
+      await updateEmail(auth.currentUser, email);
+    }
+
+    // Update user password
+    if (password) {
+      await updatePassword(auth.currentUser, password);
+    }
+
+    // Update user document in Firestore
+    await setDoc(doc(db, "users", uid), {
+      username: username.toLowerCase(),
+      email: email || decoded.email,
+      userid: uid,
+    });
+
+    res.status(200).send({ message: "User data updated successfully" });
+  } catch (error) {
+    console.error("Error updating user data:", error);
+    res.status(500).send({ error: error.message });
+  }
+});
+
+app.delete("/delete", async (req, res) => {
+  const { token } = req.body;
+
+  try {
+    const decoded = jwt.verify(token, secretKey);
+    const { uid } = decoded;
+
+    // Delete user document from Firestore
+    await deleteDoc(doc(db, "users", uid));
+
+    // Delete user account
+    await deleteUser(auth.currentUser);
+
+    res.status(200).send({ message: "User account deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting user account:", error);
+    res.status(500).send({ error: error.message });
+  }
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
