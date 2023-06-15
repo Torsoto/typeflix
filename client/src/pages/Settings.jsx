777a6b5cae0ee7664e18685984132b74347d72b9
Yunus Kara <yunuskr40@gmail.com>
2023-06-15 23:17:29 +0200

    changed design of the settings

--- client/src/pages/Settings.jsx
+++ client/src/pages/Settings.jsx
@@ -59,38 +59,48 @@ function Settings() {
       <div className="main-bg">
         <div className="h-[90%] m-auto max-w-7xl">
           <Navbar />
-          <h1 className="text-white mt-8 text-xl pb-7">Settings</h1>
-          <div className="container mx-auto bg-gray-600 p-8 rounded-lg flex">
-            <div className="w-1/2">
-              <div className="flex items-center mb-4">
-                <div className="w-16 h-16 rounded-full overflow-hidden mr-4">
+          <div className="container mx-auto">
+            <h1 className="text-white mt-8 text-xl pb-7">Settings</h1>
+            <div className="mx-auto bg-gray-600 p-8 rounded-lg flex flex-col md:flex-row">
+              <div className="mb-4 md:mb-0 md:w-1/2 flex flex-col items-start">
+                <div className="w-16 h-16 rounded-full overflow-hidden">
                   {avatarStyle && <img src={avatarStyle} alt="Avatar" />}
                 </div>
-                <div>
-                  <p className="text-white">
-                    Username: {userData && <p>{userData.username}</p>}
+                <button
+                  className="bg-black text-white font-semibold py-2 px-4 rounded-full mt-2"
+                  onClick={handleToggleAvatarOptions}
+                >
+                  Change Avatar
+                </button>
+                <button
+                  className="mt-4 bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded-full"
+                  onClick={() => handleDeleteAccount()}
+                >
+                  Delete Account
+                </button>
+              </div>
+              <div className="md:w-1/2 flex flex-col">
+                <div className="flex items-center">
+                  <p className="text-white mr-2">
+                    Username: {userData && <span>{userData.username}</span>}
                   </p>
                   <button
-                    className="bg-black text-white font-semibold py-1 px-2 rounded mt-2"
+                    className="bg-black text-white font-semibold py-1 px-2 rounded-full"
                     onClick={handleUpdateUsername}
                   >
                     <BiEdit />
                   </button>
                 </div>
-              </div>
 
-              <div className="flex items-center">
-                <div>
+                <div className="flex flex-col items-start mt-4">
                   <p className="text-white">
-                    Email: {userData && <p>{userData.email}</p>}
+                    Email: {userData && <span>{userData.email}</span>}
                   </p>
                 </div>
-              </div>
-              <div className="flex items-center">
-                <div>
-                  <p className="text-white">Password</p>
+                <div className="flex items-center mt-4">
+                  <p className="text-white mr-2">Change password</p>
                   <button
-                    className="bg-black text-white font-semibold py-1 px-2 rounded mt-2"
+                    className="bg-black text-white font-semibold py-1 px-2 rounded-full"
                     onClick={handleUpdatePassword}
                   >
                     <BiEdit />
@@ -98,34 +108,18 @@ function Settings() {
                 </div>
               </div>
             </div>
-            <div className="w-1/2">
-              <div className="flex flex-col mt-4">
-                <button
-                  className="bg-black text-white font-semibold py-2 px-4 rounded"
-                  onClick={handleToggleAvatarOptions}
-                >
-                  Change Avatar
-                </button>
-                <button
-                  className="mt-4 bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded"
-                  onClick={() => handleDeleteAccount()}
-                >
-                  Delete Account
-                </button>
-              </div>
-            </div>
+            {showAvatarOptions && (
+              <Avatars
+                handleSelectAvatar={handleSelectAvatar}
+                handleToggleAvatarOptions={handleToggleAvatarOptions}
+              />
+            )}
+            {error && <div className="text-red-500 mt-4">{error}</div>}
           </div>
-          {showAvatarOptions && (
-            <Avatars
-              handleSelectAvatar={handleSelectAvatar}
-              handleToggleAvatarOptions={handleToggleAvatarOptions}
-            />
-          )}
-          {error && <div className="text-red-500 mt-4">{error}</div>}
         </div>
         <div>
           <a href="/">
-            <span className="ont-medium text-white transition-colors hover:text-gray-400">
+            <span className="font-medium text-white transition-colors hover:text-gray-400">
               Go Back
             </span>
           </a>
