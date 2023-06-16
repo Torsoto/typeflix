797470b7ca87705add5c19e87c38e353329b156d
YusufYrkl <yusuf.yuerekli@stud.fh-campuswien.ac.at>
2023-06-17 01:40:39 +0200

    Worked on Profile

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,51 +3,74 @@ import "../styles/Home.css";
 import Navbar from "../components/UI/Navbar.jsx";
 import AuthContext from "../components/context/AuthContext.jsx";
 import "../styles/Profile.css";
+import userIcon from "../assets/profile.svg";
 
 function Profile() {
-  const [userData, setUserData] = useState(null);
-  const { userId } = useContext(AuthContext);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(`http://localhost:3000/${userId}`);
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        const data = await response.json();
-        setUserData(data);
-      } catch (error) {
-        console.error(
-          "There has been a problem with your fetch operation: ",
-          error.message
-        );
-      }
-    };
-    fetchData();
-  }, [userId]);
+  const { userData } = useContext(AuthContext);
 
   return (
     <>
       <div className="main-bg">
         <div className="h-[90%] m-auto max-w-7xl">
           <Navbar />
-          <div>
-            {/* Here you can use the user data. For example: */}
-            {userData && <p>Welcome, {userData.username}!</p>}
-          </div>{" "}
-          <div style={{ backgroundColor: "gray", padding: "20px" }}>
-            <div style={{ display: "flex" }}>
-              <div style={{ marginLeft: "20px" }}>
-                <p>Best WPM: </p>
-                <p>Avg. WPM: </p>
-                <p>Accuracy: </p>
-                <p>Games Played: </p>
-                <p>Duels Win/Lose Ratio:</p>
-                <p>Bosses Defeated: </p>
+          <div className="flex">
+            <div>
+              {userData && <h1>{userData.username}</h1>}
+              <div className="flex">
+                <div class="container  p-8 rounded-lg profil-box-bg">
+                  <div style={{ display: "flex" }}>
+                    <div style={{ textAlign: "center" }}>
+                      <img
+                        src={userIcon}
+                        alt="Avatar"
+                        style={{ width: "120px", height: "120px" }}
+                      />
+                      <h3 style={{ padding: "px" }}>X followers</h3>
+                      <button className="px-2 py-1 font-semibold text-white bg-black rounded-full">
+                        Follow
+                      </button>
+                    </div>
+                    <div style={{ display: "flex" }}>
+                      <div style={{ marginLeft: "50px" }}>
+                        <p>Best WPM: </p>
+                        <p>Avg. WPM: </p>
+                        <p>Games Played: </p>
+                        <p>Levels Completed: </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div>
+                    <h2>Last Played:</h2>
+                  </div>
+                </div>
               </div>
             </div>
-            <p style={{ marginTop: "20px" }}>Last Played:</p>
+            <div class="friends-container">
+              <h1>Friends</h1>
+              <div class="friends">
+                <div class="user-avatar">
+                  <img
+                    src={userIcon}
+                    alt="Avatar"
+                    style={{ width: "40px", height: "40px" }}
+                  />
+                </div>
+                <div class="user-avatar">
+                  <img
+                    src={userIcon}
+                    alt="Avatar"
+                    style={{ width: "40px", height: "40px" }}
+                  />
+                </div>
+                <div class="user-avatar">
+                  <img
+                    src={userIcon}
+                    alt="Avatar"
+                    style={{ width: "40px", height: "40px" }}
+                  />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
