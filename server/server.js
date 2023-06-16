1af2228ae0669836a70a77dbbba5bbdc0791478b
d3vote <65717310+d3vote@users.noreply.github.com>
2023-06-16 02:01:52 +0200

    weather api + time api

--- server/server.js
+++ server/server.js
@@ -24,6 +24,7 @@ import "./db/firebase.mjs";
 import cors from "cors";
 import crypto from "crypto";
 import jwt from "jsonwebtoken";
+import * as https from "https";
 
 const app = express();
 const auth = getAuth();
@@ -364,6 +365,69 @@ app.delete("/delete", async (req, res) => {
     res.status(500).send({ error: error.message });
   }
 });
+
+app.get('/weather/vienna', (req, res) => {
+    const options = {
+        hostname: 'weather.visualcrossing.com',
+        path: '/VisualCrossingWebServices/rest/services/timeline/Vienna?unitGroup=metric&key=UKYSD9QUBK9XXZVU3JVK9VTFG&contentType=json',
+        method: 'GET',
+    };
+
+    const request = https.request(options, (response) => {
+        let data = '';
+
+        response.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        response.on('end', () => {
+            const weatherData = JSON.parse(data);
+            const currentConditions = weatherData.days[0].conditions;
+            const temperature = weatherData.days[0].temp;
+            const description = weatherData.days[0].description;
+
+            res.status(200).json({ currentConditions, temperature, description });
+        });
+    });
+
+    request.on('error', (error) => {
+        console.error('Error retrieving weather data:', error);
+        res.status(500).send({ error: error.message });
+    });
+
+    request.end();
+});
+
+app.get('/time/vienna', (req, res) => {
+    const options = {
+        hostname: 'worldtimeapi.org',
+        path: '/api/timezone/Europe/Vienna',
+        method: 'GET',
+    };
+
+    const request = https.request(options, (response) => {
+        let data = '';
+
+        response.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        response.on('end', () => {
+            const timeData = JSON.parse(data);
+            const datetime = timeData.datetime;
+
+            res.status(200).json({ datetime });
+        });
+    });
+
+    request.on('error', (error) => {
+        console.error('Error retrieving time data:', error);
+        res.status(500).send({ error: error.message });
+    });
+
+    request.end();
+});
+
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
