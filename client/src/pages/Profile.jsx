3d42af87561170a0a3a4c86f47787866cfdbe43d
d3vote <65717310+d3vote@users.noreply.github.com>
2023-06-17 02:21:08 +0200

    Fixed usage of Profile.css over all files

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,8 +1,8 @@
+import "../styles/Profile.css";
 import React, { useContext, useEffect, useState } from "react";
 import "../styles/Home.css";
 import Navbar from "../components/UI/Navbar.jsx";
 import AuthContext from "../components/context/AuthContext.jsx";
-import "../styles/Profile.css";
 import userIcon from "../assets/profile.svg";
 
 function Profile() {
@@ -13,11 +13,11 @@ function Profile() {
       <div className="main-bg">
         <div className="h-[90%] m-auto max-w-7xl">
           <Navbar />
-          <div className="flex">
+          <div className="flex main-container">
             <div>
-              {userData && <h1>{userData.username}</h1>}
+              {userData && <h1 className="h1-s font-medium">{userData.username}</h1>}
               <div className="flex">
-                <div class="container  p-8 rounded-lg profil-box-bg">
+                <div className="min-w-[550px] min-h-[400px] p-8 rounded-lg profil-box-bg">
                   <div style={{ display: "flex" }}>
                     <div style={{ textAlign: "center" }}>
                       <img
@@ -25,13 +25,13 @@ function Profile() {
                         alt="Avatar"
                         style={{ width: "120px", height: "120px" }}
                       />
-                      <h3 style={{ padding: "px" }}>X followers</h3>
-                      <button className="px-2 py-1 font-semibold text-white bg-black rounded-full">
+                      <h3 className="h3-s" style={{ padding: "px" }}>X followers</h3>
+                      <button className="button-s px-2 py-1 font-semibold text-white bg-black rounded-full">
                         Follow
                       </button>
                     </div>
                     <div style={{ display: "flex" }}>
-                      <div style={{ marginLeft: "50px" }}>
+                      <div className="values-container" style={{ marginLeft: "50px" }}>
                         <p>Best WPM: </p>
                         <p>Avg. WPM: </p>
                         <p>Games Played: </p>
@@ -40,29 +40,29 @@ function Profile() {
                     </div>
                   </div>
                   <div>
-                    <h2>Last Played:</h2>
+                    <h2 className="h2-s">Last Played:</h2>
                   </div>
                 </div>
               </div>
             </div>
-            <div class="friends-container">
-              <h1>Friends</h1>
-              <div class="friends">
-                <div class="user-avatar">
+            <div className="friends-container">
+              <h1 className="h1-s font-medium">Friends</h1>
+              <div className="friends">
+                <div className="user-avatar">
                   <img
                     src={userIcon}
                     alt="Avatar"
                     style={{ width: "40px", height: "40px" }}
                   />
                 </div>
-                <div class="user-avatar">
+                <div className="user-avatar">
                   <img
                     src={userIcon}
                     alt="Avatar"
                     style={{ width: "40px", height: "40px" }}
                   />
                 </div>
-                <div class="user-avatar">
+                <div className="user-avatar">
                   <img
                     src={userIcon}
                     alt="Avatar"
