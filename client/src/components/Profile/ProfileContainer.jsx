8564c96aa7902d7ec409ab9415aecdc19c9d3757
d3vote <65717310+d3vote@users.noreply.github.com>
2023-06-19 17:08:09 +0200

    idk

--- client/src/components/Profile/ProfileContainer.jsx
+++ client/src/components/Profile/ProfileContainer.jsx
@@ -5,7 +5,7 @@ import userIcon from "../../assets/profile.svg";
 
 export const ProfileContainer = (username) => {
   const { fetchData, userData, avatarUrl, updateUserData } =
-    useContext(AuthContext);
+      useContext(AuthContext);
   const [customUserData, setCustomUserData] = useState("");
   const [isSameProfile, setIsSameProfile] = useState(false);
   const [following, setFollowing] = useState([]);
@@ -32,9 +32,9 @@ export const ProfileContainer = (username) => {
 
   useEffect(() => {
     fetch("http://localhost:3000/getFollowing?username=" + username.username)
-      .then((response) => response.json())
-      .then((data) => setFollowing(data))
-      .catch((e) => console.error(e));
+        .then((response) => response.json())
+        .then((data) => setFollowing(data))
+        .catch((e) => console.error(e));
   }, []);
 
   useEffect(() => {
@@ -85,11 +85,11 @@ export const ProfileContainer = (username) => {
 
   useEffect(() => {
     Promise.all(
-      following.map((followingUser) =>
-        fetch(`http://localhost:3000/getAvatar?username=${followingUser}`)
-          .then((response) => response.text())
-          .then((data) => ({ following: followingUser, avatar: data }))
-      )
+        following.map((followingUser) =>
+            fetch(`http://localhost:3000/getAvatar?username=${followingUser}`)
+                .then((response) => response.text())
+                .then((data) => ({ following: followingUser, avatar: data }))
+        )
     ).then((data) => {
       setFollowingAvatars(data);
       console.log(data);
@@ -97,79 +97,79 @@ export const ProfileContainer = (username) => {
   }, [following]);
 
   return (
-    <div className="flex main-container justify-center">
-      <div>
-        <h1 className="h1-s font-medium">{customUserData.username}</h1>
-        <div className="flex">
-          <div className="min-w-[550px] min-h-[400px] p-8 rounded-lg profil-box-bg">
-            <div className="flex">
-              <div className="text-center flex justify-around flex-col">
-                <img
-                  src={avatarUrl ? userIcon : plainProfileImage}
-                  alt="Avatar"
-                  className="rounded-full w-[120px] h-[120px] bg-white"
-                />
-                <h3 className="h3-s py-[10px]">0 followers</h3>
-                {!isSameProfile && (
-                  <button
-                    className={`button-s px-2 py-1 font-semibold ${
-                      isFollowing
-                        ? "bg-white text-black"
-                        : "bg-black text-white"
-                    } rounded-full`}
-                    onClick={isFollowing ? handleUnfollow : handleFollow}
-                  >
-                    {isFollowing ? "Unfollow" : "Follow"}
-                  </button>
-                )}
-              </div>
+      <div className="flex main-container justify-center">
+        <div>
+          <h1 className="h1-s font-medium">{customUserData.username}</h1>
+          <div className="flex">
+            <div className="min-w-[550px] min-h-[400px] p-8 rounded-lg profil-box-bg">
               <div className="flex">
-                <div className="values-container ml-[50px]">
-                  <p>Best WPM: {customUserData.bestwpm}</p>
-                  <p>
-                    Avg. WPM:{" "}
-                    {customUserData.avgwpm !== undefined
-                      ? customUserData.avgwpm.toFixed(2)
-                      : "0"}
-                  </p>
-                  <p>Games Played: {customUserData.gamesplayed}</p>
-                  <p>Levels Completed: {customUserData.themescompleted}</p>
+                <div className="text-center flex justify-around flex-col">
+                  <img
+                      src={avatarUrl ? userIcon : plainProfileImage}
+                      alt="Avatar"
+                      className="rounded-full w-[120px] h-[120px] bg-white"
+                  />
+                  <h3 className="h3-s py-[10px]">0 followers</h3>
+                  {!isSameProfile && (
+                      <button
+                          className={`button-s px-2 py-1 font-semibold ${
+                              isFollowing
+                                  ? "bg-white text-black"
+                                  : "bg-black text-white"
+                          } rounded-full`}
+                          onClick={isFollowing ? handleUnfollow : handleFollow}
+                      >
+                        {isFollowing ? "Unfollow" : "Follow"}
+                      </button>
+                  )}
+                </div>
+                <div className="flex">
+                  <div className="values-container ml-[50px]">
+                    <p>Best WPM: {customUserData.bestwpm}</p>
+                    <p>
+                      Avg. WPM:{" "}
+                      {customUserData.avgwpm !== undefined
+                          ? customUserData.avgwpm.toFixed(2)
+                          : "0"}
+                    </p>
+                    <p>Games Played: {customUserData.gamesplayed}</p>
+                    <p>Levels Completed: {customUserData.themescompleted}</p>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div>
-              <h2 className="h2-s">Last Played:</h2>
+              <div>
+                <h2 className="h2-s">Last Played:</h2>
+              </div>
             </div>
           </div>
         </div>
+        {following.length > 0 && (
+            <div className="following-container">
+              <h1 className="h1-s font-medium">Following</h1>
+              <div className="following">
+                {following.map((followingUser) => {
+                  const followingAvatar = followingAvatars.find(
+                      (item) => item.following === followingUser
+                  )?.avatar;
+                  return (
+                      <div
+                          key={followingUser}
+                          className="user-avatar flex text-white gap-[12px]"
+                      >
+                        <img
+                            src={followingAvatar || plainProfileImage}
+                            alt="Avatar"
+                            className="rounded-full w-[50px] h-[50px] bg-white"
+                        />
+                        <a href={`/${followingUser}`}>
+                          <p className="mt-[9px] text-xl">{followingUser}</p>
+                        </a>
+                      </div>
+                  );
+                })}
+              </div>
+            </div>
+        )}
       </div>
-      {following.length > 0 && (
-        <div className="friends-container">
-          <h1 className="h1-s font-medium">Following</h1>
-          <div className="friends">
-            {following.map((followingUser) => {
-              const followingAvatar = followingAvatars.find(
-                (item) => item.following === followingUser
-              )?.avatar;
-              return (
-                <div
-                  key={followingUser}
-                  className="user-avatar flex text-white gap-[12px]"
-                >
-                  <img
-                    src={followingAvatar || plainProfileImage}
-                    alt="Avatar"
-                    className="rounded-full w-[50px] h-[50px] bg-white"
-                  />
-                  <a href={`/${followingUser}`}>
-                    <p className="mt-[9px] text-xl">{followingUser}</p>
-                  </a>
-                </div>
-              );
-            })}
-          </div>
-        </div>
-      )}
-    </div>
   );
 };
