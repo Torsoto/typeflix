066af9e56e12ceb071ce1a0838604529296f53f8
d3vote <65717310+d3vote@users.noreply.github.com>
2023-06-22 02:39:29 +0200

    Form validation on Signup

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -12,13 +12,21 @@ function Signup() {
   const [username, setUsername] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [emailError, setEmailError] = useState(false);
-  const [loading, setLoading] = useState(false); // new loading state
+  const [usernameError, setUsernameError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [confirmPasswordError, setConfirmPasswordError] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [notification, setNotification] = useState({
     show: false,
     message: "",
   });
   const { login } = useContext(AuthContext);
 
+  // Validates email format:
+  // - Starts with one or more alphanumeric characters, dots, underscores, percentage signs, or plus signs
+  // - Followed by the at symbol
+  // - Followed by one or more alphanumeric characters, dots, or dashes
+  // - Ends with a dot and at least two alphabetic characters
   const validateEmail = (e) => {
     setEmail(e.target.value);
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
@@ -27,13 +35,55 @@ function Signup() {
     }
   };
 
+  // Validates username format:
+  // - Allows alphanumeric characters, dots, and underscores
+  // - Must be between 3 and 20 characters long
+  // - Cannot have consecutive dots or underscores
+  // - Cannot start or end with a dot or underscore
+  const validateUsername = (e) => {
+    setUsername(e.target.value);
+    const usernameRegex = /^(?=[a-zA-Z0-9._]{3,20}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+    if (e.target.value.length > 0) {
+      setUsernameError(!usernameRegex.test(e.target.value));
+    }
+  };
+
+  // Validates password format:
+  // - Must contain at least one lowercase letter, one uppercase letter, and one digit
+  // - Allows alphanumeric characters and dashes
+  // - Must be at least 8 characters long
+  const validatePassword = (e) => {
+    setPassword(e.target.value);
+    if(1 === 2){ // REMOVE THE IF STATEMENT BEFORE DEPLOYING
+      const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d-]{8,}$/;
+      setPasswordError(!passwordRegex.test(e.target.value));
+    }
+    setConfirmPasswordError(confirmPassword !== password);
+  };
+
+  const validateConfirmPassword = (e) => {
+    setConfirmPassword(e.target.value);
+    setConfirmPasswordError(e.target.value !== password);
+  };
+
   const handleSignup = async () => {
     if (password !== confirmPassword) {
       alert("Passwords do not match");
       return;
     }
 
-    setLoading(true); // start loading
+    if(usernameError) {
+      alert("Invalid username");
+      return;
+    }
+
+    if(passwordError) {
+      alert("Invalid password");
+      return;
+    }
+
+    setLoading(true);
+
     try {
       const response = await fetch("http://localhost:3000/signup", {
         method: "POST",
@@ -60,8 +110,8 @@ function Signup() {
       setNotification({
         show: true,
         message:
-          ERROR_MAP[error.message] ||
-          "An error occurred during signup. Please try again.",
+            ERROR_MAP[error.message] ||
+            "An error occurred during signup. Please try again.",
       });
     } finally {
       setLoading(false); // stop loading regardless of success or error
@@ -69,89 +119,98 @@ function Signup() {
   };
 
   return (
-    <>
-      <div className="main-bg">
-        <div className="m-auto max-w-7xl">
-          <a href="/">
-            <LogoNav />
-          </a>
-          <div
-            className="flex-col justify-center max-w-md mx-auto my-3"
-            style={{
-              height: "calc(100vh - 180px)",
-            }}
-          >
-            <h1 className="py-3 mb-10 text-3xl font-bold leading-10 text-left text-white align-top">
-              Sign up
-            </h1>
-            <h1 className="my-2 text-base font-medium leading-8 text-left text-white align-top">
-              Username
-            </h1>
-            <input
-              type="text"
-              className="w-full px-4 py-3 my-2 text-black bg-white rounded-full"
-              placeholder="Enter your username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <h1 className="my-2 text-base font-medium leading-8 text-left text-white align-top">
-              Email
-            </h1>
-            <input
-              type="email"
-              className={`w-full px-4 py-3 my-2 text-black bg-white rounded-full transition-all duration-200 ${
-                emailError ? "email-input-error" : ""
-              }`}
-              placeholder="Enter your email"
-              value={email}
-              onChange={validateEmail}
-            />
-            {emailError && <p className="text-red-500">Invalid Email</p>}
-            <h1 className="my-2 text-base font-medium leading-8 text-left text-white align-top">
-              Password
-            </h1>
-            <input
-              type="password"
-              className="w-full px-4 py-3 my-2 text-black bg-white rounded-full"
-              placeholder="Enter your password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <h1 className="my-2 text-base font-medium leading-8 text-left text-white align-top">
-              Confirm Password
-            </h1>
-            <input
-              type="password"
-              className="w-full px-4 py-3 my-2 mb-5 text-black bg-white rounded-full"
-              placeholder="Confirm your password"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-            />
-            <button
-              onClick={handleSignup}
-              className="flex items-center justify-center w-full py-3 my-8 font-bold text-white bg-black rounded-full"
-              disabled={loading}
+      <>
+        <div className="main-bg">
+          <div className="m-auto max-w-7xl">
+            <a href="/">
+              <LogoNav />
+            </a>
+            <div
+                className="flex-col justify-center max-w-md mx-auto my-3"
+                style={{
+                  height: "calc(100vh - 180px)",
+                }}
             >
-              {loading ? (
-                <div className="h-[24px]">
-                  <BeatLoader color="#FFFFFF" size={8} />
-                </div>
-              ) : (
-                "Sign up"
-              )}
-            </button>
-            <div className="flex items-center justify-center "></div>
-            <div className="flex items-center justify-center ">
-              <span className="text-gray-500">Already have an account?</span>
-              <a href="/login" className="ml-2 text-white">
-                Log in
-              </a>
+              <h1 className="py-3 mb-10 text-3xl font-bold leading-10 text-left text-white align-top">
+                Sign up
+              </h1>
+              <h1 className="my-2 text-base font-medium leading-8 text-left text-white align-top">
+                Username
+              </h1>
+              <input
+                  type="text"
+                  className={`w-full px-4 py-3 my-2 text-black bg-white rounded-full transition-all duration-200 ${
+                      usernameError ? "input-error" : ""
+                  }`}
+                  placeholder="Enter your username"
+                  value={username}
+                  onChange={validateUsername}
+              />
+              {usernameError && <p className="text-red-500">Invalid Username</p>}
+              <h1 className="my-2 text-base font-medium leading-8 text-left text-white align-top">
+                Email
+              </h1>
+              <input
+                  type="email"
+                  className={`w-full px-4 py-3 my-2 text-black bg-white rounded-full transition-all duration-200 ${
+                      emailError ? "input-error" : ""
+                  }`}
+                  placeholder="Enter your email"
+                  value={email}
+                  onChange={validateEmail}
+              />
+              {emailError && <p className="text-red-500">Invalid Email</p>}
+              <h1 className="my-2 text-base font-medium leading-8 text-left text-white align-top">
+                Password
+              </h1>
+              <input
+                  type="password"
+                  className={`w-full px-4 py-3 my-2 text-black bg-white rounded-full transition-all duration-200 ${
+                      passwordError ? "input-error" : ""
+                  }`}
+                  placeholder="Enter your password"
+                  value={password}
+                  onChange={validatePassword}
+              />
+              {passwordError && <p className="text-red-500">Invalid Password. Must contain at least 8 characters, one uppercase, one lowercase, one number and one special character.</p>}
+              <h1 className="my-2 text-base font-medium leading-8 text-left text-white align-top">
+                Confirm Password
+              </h1>
+              <input
+                  type="password"
+                  className={`w-full px-4 py-3 my-2 mb-5 text-black bg-white rounded-full transition-all duration-200 ${
+                      confirmPasswordError ? "input-error" : ""
+                  }`}
+                  placeholder="Confirm your password"
+                  value={confirmPassword}
+                  onChange={validateConfirmPassword}
+              />
+              {confirmPasswordError && <p className="text-red-500">Passwords do not match.</p>}
+              <button
+                  onClick={handleSignup}
+                  className="flex items-center justify-center w-full py-3 my-8 font-bold text-white bg-black rounded-full"
+                  disabled={loading}
+              >
+                {loading ? (
+                    <div className="h-[24px]">
+                      <BeatLoader color="#FFFFFF" size={8} />
+                    </div>
+                ) : (
+                    "Sign up"
+                )}
+              </button>
+              <div className="flex items-center justify-center "></div>
+              <div className="flex items-center justify-center ">
+                <span className="text-gray-500">Already have an account?</span>
+                <a href="/login" className="ml-2 text-white">
+                  Log in
+                </a>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      {notification.show && <Notification message={notification.message} />}
-    </>
+        {notification.show && <Notification message={notification.message} />}
+      </>
   );
 }
 
